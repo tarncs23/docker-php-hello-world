a44f384ec199506ece4631ec80c1d26261895c16
Tarn P <yaowaluk@e-bookstudio.com>
2022-01-03 16:21:21 +0700

    first commit
    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>
+		Website
+	</title>
+	<style>
+	@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@100&display=swap');
+	* { font-family: 'Kanit', sans-serif; font-size: 30px; color:#002DA7; }
+	table { height: 50%; border: 0px solid gray;}
+	td { text-align: center; padding: 50;}
+
+	
+
+	</style>
+</head>
+<body>
+	<center>		
+		<table>
+			<tr><td><br/></td></tr>
+			<tr><td><?php echo "Hello World" ?></td></tr>
+			
+		</table>
+	</center>
+</body>
+
+</html>
